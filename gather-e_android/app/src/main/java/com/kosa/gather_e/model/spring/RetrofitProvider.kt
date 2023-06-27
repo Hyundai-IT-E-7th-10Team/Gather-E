9b06d4014885b4b306fb345acaab86d381224e5b
kosa-kww <dyffh1031@gmail.com>
2023-06-27 23:30:45 +0900

    feat: [Retrofit] spring과 통신을 위한 retrofit 객체 생성

---
+++ gather-e_android/app/src/main/java/com/kosa/gather_e/model/spring/RetrofitProvider.kt
@@ -0,0 +1,17 @@
+package com.kosa.gather_e.model.spring
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitProvider {
+    private const val BASE_URL = "http://10.0.2.2:8080/api/"
+
+    private val retrofit =  Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun getRetrofit(): SpringInterface {
+        return retrofit.create(SpringInterface::class.java);
+    }
+}
