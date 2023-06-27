e3605afca5d27112b34d2988f32906e218acc50f
seolhi <seolhi1212@gmail.com>
2023-06-27 14:26:26 +0900

    feat: [searchLocationAPIService] 장소 리스트 검색 - ksh

--- gather-e_android/app/src/main/java/com/kosa/gather_e/ui/map/MapFragment.kt
+++ gather-e_android/app/src/main/java/com/kosa/gather_e/ui/map/MapFragment.kt
@@ -9,15 +9,20 @@ import androidx.lifecycle.ViewModelProvider
 import com.kosa.gather_e.R
 import com.kosa.gather_e.databinding.FragmentMapBinding
 import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.NaverMapSdk
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var binding: FragmentMapBinding
+
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,17 +45,46 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                 fm.beginTransaction().add(R.id.map_fragment, it).commit()
             }
 
+        locationSource =
+            FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
         mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(naverMap: NaverMap) {
-        val uiSettings = naverMap.uiSettings
-        // 현위치 버튼
-        uiSettings.isLocationButtonEnabled = true
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
 
-        // 마커 위도경도로 띄우기
-        val marker = Marker()
-        marker.position = LatLng(37.5670135, 126.9783740)
-        marker.map = naverMap
+    override fun onMapReady(naverMap: NaverMap) {
+
+        val locationOverlay = naverMap.locationOverlay
+        locationOverlay.isVisible = true
+
+
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+
+
+//        val uiSettings = naverMap.uiSettings
+//        // 현위치 버튼
+//        uiSettings.isLocationButtonEnabled = true
+//
+//        // 마커 위도경도로 띄우기
+//        val marker = Marker()
+//        marker.position = LatLng(37.5670135, 126.9783740)
+//        marker.map = naverMap
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
