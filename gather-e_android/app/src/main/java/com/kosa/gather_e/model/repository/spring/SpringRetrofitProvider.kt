81c9e75dfad007628988695b09659e0808214937
kosa-kww <dyffh1031@gmail.com>
2023-06-29 17:04:33 +0900

    feat: [마이페이지] 마이 페이지 개인 정보 불러오기 및 로그아웃 구현

--- gather-e_android/app/src/main/java/com/kosa/gather_e/model/repository/spring/SpringRetrofitProvider.kt
+++ gather-e_android/app/src/main/java/com/kosa/gather_e/model/repository/spring/SpringRetrofitProvider.kt
@@ -1,5 +1,6 @@
 package com.kosa.gather_e.model.repository.spring
 
+import android.util.Log
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -7,26 +8,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 object SpringRetrofitProvider {
     private const val BASE_URL = "http://10.0.2.2:8080/api/"
 
-
-    // 우원오빠가 짠거 주석처리
-/*    private val retrofit =  Retrofit.Builder()
+    private var retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
-        .build()*/
-
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .client(createOkHttpClient())
         .build()
 
     // Bearer 토큰을 추가
-    private fun createOkHttpClient(): OkHttpClient {
+    private fun createOkHttpClient(token: String): OkHttpClient {
         val httpClient = OkHttpClient.Builder()
-
-        // string.xml에 넣어야 하는데 얘는 fragment같은게 아니라서 어려움 ㅠ
-        val token = ""
-
         httpClient.addInterceptor { chain ->
             val request = chain.request().newBuilder()
                 .header("Authorization", "Bearer $token") // Bearer 토큰을 헤더에 추가
@@ -36,6 +25,14 @@ object SpringRetrofitProvider {
         return httpClient.build()
     }
 
+    fun init(token: String) {
+        Log.d("gather", "초기화를 해보자")
+        retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(createOkHttpClient(token))
+            .build()
+    }
 
     fun getRetrofit(): SpringInterface {
         return retrofit.create(SpringInterface::class.java);
