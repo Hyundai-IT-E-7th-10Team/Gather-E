e701d4d2e743f33755140cb445cadb73a7e90558
seolhi <seolhi1212@gmail.com>
2023-06-29 09:18:31 +0900

    feat: [글작성] 카테고리 버튼 동적 생성 - ksh

--- gather-e_android/app/src/main/java/com/kosa/gather_e/WriteActivity.kt
+++ gather-e_android/app/src/main/java/com/kosa/gather_e/WriteActivity.kt
@@ -5,17 +5,28 @@ import android.app.Activity
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.icu.util.Calendar
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat.getCategory
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kosa.gather_e.model.entity.location.SearchLocationEntity
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.kosa.gather_e.model.entity.chat.ChatListItem
 import com.kosa.gather_e.databinding.ActivityWriteBinding
+import com.kosa.gather_e.model.entity.category.CategoryEntity
+import com.kosa.gather_e.model.repository.spring.SpringRetrofitProvider
 import com.kosa.gather_e.ui.searchlocation.SearchLocationActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class WriteActivity : AppCompatActivity() {
@@ -34,8 +45,66 @@ class WriteActivity : AppCompatActivity() {
         }
         // toolbar의 작성 완료 버튼
         binding.completeBtn.setOnClickListener {
+            // 완료 버튼 누르면 채팅방 생성
+            val chatRoom = ChatListItem(
+                userId = "user01",
+                itemTitle = "chatRoom02",
+                key = System.currentTimeMillis()
+            )
+            val chatDB = Firebase.database.reference.child("Chats")
+            val newChatRoomRef = chatDB.push()
+
+            newChatRoomRef.setValue(chatRoom)
+                .addOnSuccessListener {
+                }
+                .addOnFailureListener { error ->
+                }
+
+            // 완료 버튼 누르면 글 작성 완료
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val selectedLocation = binding.placeText.text.toString()
+            val selectedDate = binding.dateText.text.toString()
+            val selectedTime = binding.timeText.text.toString()
+            val selectedPersonnel = binding.personnelNumberPicker.value
+
+            // 데이터 유효성 검사 (예: 제목과 내용은 비어있으면 안됨)
+            if (title.isBlank() || content.isBlank()) {
+                // 유효성 검사에 실패하면 사용자에게 알림
+                Toast.makeText(this, "제목과 내용을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
         }
+
+        // 카테고리 선택 버튼 동적으로 생성
+        val callCategoryList: Call<List<CategoryEntity>> = SpringRetrofitProvider.getRetrofit().getCategory()
+        callCategoryList.enqueue(object : Callback<List<CategoryEntity>> {
+            override fun onResponse(call: Call<List<CategoryEntity>>, response: Response<List<CategoryEntity>>
+            ) {
+                Log.d("gather", "$call, $response") // 403 에러 떠서 retrofitProvider에 bearear 토큰 추가
+                if(response.isSuccessful) {
+                    val categoryList = response.body()
+                    categoryList?.let { categories ->
+                        val buttonContainer: LinearLayout = findViewById(R.id.buttonContainer)
+
+                        for (category in categories){
+                            val button = Button(this@WriteActivity)
+                            button.text = category.categoryName
+                            button.id = category.categorySeq
+
+                            buttonContainer.addView(button)
+                        }
+
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<CategoryEntity>>, t: Throwable) {
+                Log.d("gather", "$t")
+            }
+        })
+
 
         // 만나는 날짜 - Calendar
         val calendar = Calendar.getInstance()
@@ -91,30 +160,5 @@ class WriteActivity : AppCompatActivity() {
                 binding.placeText.text = selectedLocation.place_name
             }
         }
-
-
-        binding.completeBtn.setOnClickListener {
-            val chatRoom = ChatListItem(
-                userId = "user01",
-                itemTitle = "chatRoom02",
-                key = System.currentTimeMillis()
-            )
-
-            val chatDB = Firebase.database.reference.child("Chats")
-
-            val newChatRoomRef = chatDB.push()
-
-            newChatRoomRef.setValue(chatRoom)
-                .addOnSuccessListener {
-
-                }
-                .addOnFailureListener { error ->
-
-                }
-        }
-
-
-
-
     }
 }
--- gather-e_android/app/src/main/java/com/kosa/gather_e/model/entity/gather/GatherEntity.kt
+++ gather-e_android/app/src/main/java/com/kosa/gather_e/model/entity/gather/GatherEntity.kt
@@ -1,3 +1,12 @@
 package com.kosa.gather_e.model.entity.gather
 
-data class GatherEntity()
+data class GatherEntity(
+    val categorySeq: Int,
+    val categoryName: String,
+    val gatherTitle: String,
+    val gatherDate: String,
+    val gatherDescription: String,
+    val gatherLimit: Int,
+    val gatherLatitude: Double,
+    val gatherLongitude: Double
+)
