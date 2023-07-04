21bb19e047e8d21f5cf13005499603bd2e1af7cc
kosa-kww <dyffh1031@gmail.com>
2023-07-04 18:06:50 +0900

    fix: [홤화면] 버그 수정 및, 실시간 재로딩 추가 - kww

--- gather-e_android/app/src/main/java/com/kosa/gather_e/ui/home/PostDetailActivity.kt
+++ gather-e_android/app/src/main/java/com/kosa/gather_e/ui/home/PostDetailActivity.kt
@@ -1,5 +1,6 @@
 package com.kosa.gather_e.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ReportFragment.Companion.reportFragment
 import com.bumptech.glide.Glide
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.kosa.gather_e.R
 import com.kosa.gather_e.databinding.ActivityPostDetailBinding
 import com.kosa.gather_e.model.entity.gather.GatherEntity
@@ -28,30 +30,47 @@ class PostDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPostDetailBinding.inflate(layoutInflater)
         gatherEntity = intent.getSerializableExtra("gather") as GatherEntity
-        val isGathering = GatherUtil.isGathering(gatherEntity)
-        binding.backBtn.setOnClickListener { finish() }
-        with(gatherEntity) {
-            binding.categoryText.text = categoryName
-            binding.statusText.text = if (isGathering) {
-                "모집중"
-            } else {
-                "모집 종료"
-            }
-            binding.titleText.text = gatherTitle
-            binding.calendarText.text = gatherDate
-            binding.locationText.text = gatherLocationName
-            binding.contentText.text = gatherDescription
-            binding.cntText.text = "참여한 이웃 %d/%d".format(gatherUserCnt, gatherLimit)
-            Glide.with(this@PostDetailActivity).load(creatorImgUrl).into(binding.ownerProfileImg)
-            binding.ownerName.text = creatorName
-        }
 
-        val call = SpringRetrofitProvider.getRetrofit()
-            .getGatherUsers(gatherEntity.gatherSeq!!)
+        val isBefore = GatherUtil.isGathering(gatherEntity)
+        val isFull = GatherUtil.isFull(gatherEntity)
+
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
+        SpringRetrofitProvider.getRetrofit().getGatherDetail(gatherEntity.gatherSeq!!)
+            .enqueue(object : Callback<GatherEntity> {
+                override fun onResponse(
+                    call: Call<GatherEntity>, response: Response<GatherEntity>
+                ) {
+                    with(response.body()!!) {
+                        binding.categoryText.text = categoryName
+                        binding.statusText.text =
+                            if (GatherUtil.isGathering(this) && !GatherUtil.isFull(this)) {
+                                "모집중"
+                            } else {
+                                "모집 종료"
+                            }
+                        binding.titleText.text = gatherTitle
+                        binding.calendarText.text = gatherDate
+                        binding.locationText.text = gatherLocationName
+                        binding.contentText.text = gatherDescription
+                        binding.cntText.text = "참여한 이웃 %d/%d".format(gatherUserCnt, gatherLimit)
+                        Glide.with(this@PostDetailActivity).load(creatorImgUrl)
+                            .into(binding.ownerProfileImg)
+                        binding.ownerName.text = creatorName
+                    }
+                }
+
+                override fun onFailure(call: Call<GatherEntity>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+
+        val call = SpringRetrofitProvider.getRetrofit().getGatherUsers(gatherEntity.gatherSeq!!)
         call.enqueue(object : Callback<List<UserEntity>> {
             override fun onResponse(
-                call: Call<List<UserEntity>>,
-                response: Response<List<UserEntity>>
+                call: Call<List<UserEntity>>, response: Response<List<UserEntity>>
             ) {
                 var isJoined = false
                 response.body()?.forEach {
@@ -59,19 +78,16 @@ class PostDetailActivity : AppCompatActivity() {
                         isJoined = true
                     }
                 }
-                if(isJoined && isGathering) setBtnCancle()
-                else if(!isJoined && isGathering) setBtnJoin()
-                else if(isJoined && !isGathering) setBtnGone()
+                if (isJoined && isBefore) setBtnCancle()
+                else if (!isJoined && isBefore && !isFull) setBtnJoin()
+                else if (!isBefore) setBtnGone()
                 else setBtnDone()
             }
+
             override fun onFailure(call: Call<List<UserEntity>>, t: Throwable) {
             }
         })
-        if(isGathering) {
-            binding.joinBtn.setOnClickListener{
 
-            }
-        }
         setContentView(binding.root)
     }
 
@@ -79,22 +95,25 @@ class PostDetailActivity : AppCompatActivity() {
         binding.joinBtn.setOnClickListener(null)
         binding.joinBtn.visibility = View.VISIBLE
         binding.joinBtn.text = "참가 하기"
-        binding.joinBtn.setOnClickListener{
+        binding.joinBtn.backgroundTintList = ContextCompat.getColorStateList(
+            this@PostDetailActivity, R.color.light_purple
+        )
+        binding.joinBtn.setOnClickListener {
 
-            val call = SpringRetrofitProvider.getRetrofit().joinGather(UserGather(gatherEntity.gatherSeq!!.toLong(), null))
-            call.enqueue(object: Callback<UserGather> {
+            val call = SpringRetrofitProvider.getRetrofit()
+                .joinGather(UserGather(gatherEntity.gatherSeq!!.toLong(), null))
+            call.enqueue(object : Callback<UserGather> {
                 override fun onResponse(call: Call<UserGather>, response: Response<UserGather>) {
-                    Toast.makeText(this@PostDetailActivity, "모임에 참여하였습니다", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@PostDetailActivity, "모임에 참여하였습니다", Toast.LENGTH_SHORT)
+                        .show()
                     setBtnCancle()
                 }
+
                 override fun onFailure(call: Call<UserGather>, t: Throwable) {
                     t.printStackTrace()
                 }
             })
         }
-    }
-    fun setBtnGone() {
-        binding.joinBtn.visibility = View.GONE
     }
 
     fun setBtnCancle() {
@@ -102,22 +121,27 @@ class PostDetailActivity : AppCompatActivity() {
         binding.joinBtn.visibility = View.VISIBLE
         binding.joinBtn.text = "참가 취소"
         binding.joinBtn.backgroundTintList = ContextCompat.getColorStateList(
-            this@PostDetailActivity,
-            R.color.light_purple_cancled
+            this@PostDetailActivity, R.color.light_purple_cancled
         )
-        binding.joinBtn.setOnClickListener{
+        binding.joinBtn.setOnClickListener {
 
-            val call = SpringRetrofitProvider.getRetrofit().leaveGather(gatherEntity.gatherSeq!!.toLong())
-            call.enqueue(object: Callback<UserGather> {
+            val call =
+                SpringRetrofitProvider.getRetrofit().leaveGather(gatherEntity.gatherSeq!!.toLong())
+            call.enqueue(object : Callback<UserGather> {
                 override fun onResponse(call: Call<UserGather>, response: Response<UserGather>) {
-                    Toast.makeText(this@PostDetailActivity, "모임을 취소했습니다.", Toast.LENGTH_SHORT).show()
-                    setBtnCancle()
+                    Toast.makeText(this@PostDetailActivity, "모임을 취소했습니다.", Toast.LENGTH_SHORT)
+                        .show()
+                    setBtnJoin()
                 }
+
                 override fun onFailure(call: Call<UserGather>, t: Throwable) {
-                    t.printStackTrace()
                 }
             })
         }
+    }
+
+    fun setBtnGone() {
+        binding.joinBtn.visibility = View.GONE
     }
 
     fun setBtnDone() {
--- gather-e_spring/src/main/java/com/kosa/gather_e/user_gather/controller/UserGatherController.java
+++ gather-e_spring/src/main/java/com/kosa/gather_e/user_gather/controller/UserGatherController.java
@@ -4,6 +4,7 @@ import com.kosa.gather_e.auth.vo.UserVO;
 import com.kosa.gather_e.user_gather.service.UserGatherService;
 import com.kosa.gather_e.user_gather.vo.UserGatherVO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +14,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/user_gather")
 @RequiredArgsConstructor
+@Slf4j
 public class UserGatherController {
 
     private final UserGatherService userGatherService;
 
     @GetMapping("/{gatherSeq}")
     public ResponseEntity<List<UserVO>> userGather(@PathVariable long gatherSeq) {
+        log.debug("전체 유저 API 호출" + gatherSeq);
         return new ResponseEntity<>(userGatherService.getUsersInGather(gatherSeq), HttpStatus.OK);
     }
 
